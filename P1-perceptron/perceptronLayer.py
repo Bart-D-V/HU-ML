97a4e9dad44c1710acf74c84f95b6e646baabedc
Bartbabart <Bart.de.vries@outlook.com>
2022-02-18 16:50:01 +0100

    added class perceptron network and put all classes in jupyter notebook

--- P1-perceptron/perceptronLayer.py
+++
@@ -1,9 +0,0 @@
-from perceptron import Perceptron
-
-
-class PerceptronLayer:
-
-    perceptrons: list[Perceptron]
-
-    def __init__(self, perceptrons: list[Perceptron]):
-        self.perceptrons = perceptrons
--- P1-perceptron/perceptronNetwork.py
+++
@@ -1,9 +0,0 @@
-from perceptronLayer import PerceptronLayer
-
-
-class PerceptronNetwork:
-
-    perceptron_layers: list[PerceptronLayer]
-
-    def __init__(self, perceptron_layers: list[PerceptronLayer]):
-        self.perceptron_layers = perceptron_layers
