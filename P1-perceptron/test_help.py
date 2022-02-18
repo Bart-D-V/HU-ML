fd88e070c26667b86f2c856e313cb8e424642cbe
Bartbabart <Bart.de.vries@outlook.com>
2022-02-18 19:14:42 +0100

    added tests

--- P1-perceptron/test_help.py
+++ P1-perceptron/test_help.py
@@ -1,3 +1,13 @@
+def print_perceptron(perceptron):
+    for inp in all_possible_inputs(len(perceptron.weights)):
+        print("met input: " + str(inp) + " is output: " + str(perceptron.activation(inp)))
+
+
+def print_perceptron_network(perceptron_network):
+    for inp in all_possible_inputs(len(perceptron_network.perceptron_layers)):
+        print("met input: " + str(inp) + " is de output: " + str(perceptron_network.feed_forward(inp)))
+
+
 def all_possible_inputs(a_inputs):
     inputs = []
     for i in range(1 << a_inputs):
